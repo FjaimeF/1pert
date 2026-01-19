840c5187bf237382c62d745162de1381354e7985
FjaimeF <jaimepercca2@gmail.com>
2026-01-19 12:20:50 -0500

    wwwww

--- test_manual.py
+++ test_manual.py
@@ -1,5 +1,7 @@
-import automatizacion
+def sumar(a, b):
+    return a + b
 
-print("Test 1:", automatizacion.sumar(2, 3) == 5)
-print("Test 2:", automatizacion.sumar(0, 0) == 0)
-print("Test 3:", automatizacion.sumar(-1, 1) == 0)
+
+print("Test 1:", sumar(2, 3) == 5)
+print("Test 2:", sumar(0, 0) == 0)
+print("Test 3:", sumar(-1, 1) == 0)

0820c947474ed7072075f8b0a4bf546e8ca5cbc6
FjaimeF <jaimepercca2@gmail.com>
2026-01-19 12:13:10 -0500

    tttt

--- wall.py
+++ wall.py
@@ -3,22 +3,22 @@ import sys
 
 print("Automatizacion iniciada")
 
-# Variables globales innecesarias
-contador = "0"   # debería ser int
+
+contador = "0"   
 
 for i in range(5):
     print("Paso", i)
     time.sleep(1)
 
-    # Error lógico y de tipo
-    contador = contador + i   # ❌ string + int
+    
+    contador = contador + i   
 
-    # Escritura de archivo sin control
+    
     f = open("log.txt", "a")
     f.write("Paso " + str(i) + "\n")
-    # ❌ no se cierra el archivo
+    
 
-    # Error intencional en medio
+    
     if i == 2:
         print("Algo salio mal pero seguimos...")
 
